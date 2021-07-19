072230c28502eb05b4bdd81cfe17d921284af64b
albertgoncalves <goncalves.albert@gmail.com>
2021-07-19 19:39:34 -0400

    recover array index from pointer

---
+++ src/pointer_to_index.cpp
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef uint16_t u16;
+
+typedef int32_t i32;
+
+i32 main() {
+    u16 array[] = {
+        0,
+        1,
+        2,
+        3,
+        4,
+    };
+    u16* a = &array[1];
+    u16* b = &array[2];
+    u16* c = &array[4];
+    printf("%zu\n%zu\n%zu\n", a - array, b - array, c - array);
+    return EXIT_SUCCESS;
+}
