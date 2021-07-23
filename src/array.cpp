f1508a33419017bacd68b7de15a17be2d3abe038
albertgoncalves <goncalves.albert@gmail.com>
2021-07-22 22:11:37 -0400

    array struct

--- src/array.cpp
+++ src/array.cpp
@@ -7,32 +7,35 @@ typedef size_t  usize;
 
 typedef int32_t i32;
 
+template <typename T, usize N>
+struct Array {
+    T items[N];
+};
+
 static void show(u8 x) {
     printf("%hhu", x);
 }
 
 template <typename T, usize N>
-static void show(T array[N]) {
+static void show(Array<T, N>* array) {
     printf("[ ");
     for (usize i = 0; i < N; ++i) {
-        show(array[i]);
+        show(array->items[i]);
         printf(" ");
     }
     printf("]\n");
 }
 
-#define N 8
-
 i32 main() {
-    u8 array[N] = {};
-    array[0] = 1;
-    array[1] = 0;
-    array[2] = 3;
-    array[3] = 2;
-    array[4] = 5;
-    array[5] = 4;
-    array[6] = 7;
-    array[7] = 6;
-    show<u8, N>(array);
+    Array<u8, 8> array = {};
+    array.items[0] = 1;
+    array.items[1] = 0;
+    array.items[2] = 3;
+    array.items[3] = 2;
+    array.items[4] = 5;
+    array.items[5] = 4;
+    array.items[6] = 7;
+    array.items[7] = 6;
+    show(&array);
     return EXIT_SUCCESS;
 }
