3150c6a910752b2a00f353f2bff335b1dc36a989
albertgoncalves <goncalves.albert@gmail.com>
2021-08-03 19:49:42 -0400

    u64 -> u32, even faster!

--- src/cups.cpp
+++ src/cups.cpp
@@ -4,6 +4,7 @@
 
 // NOTE: See `https://adventofcode.com/2020/day/23`.
 
+typedef uint32_t u32;
 typedef uint64_t u64;
 
 typedef int32_t i32;
@@ -22,10 +23,10 @@ typedef int32_t i32;
 #define CAP 1000000
 
 struct Memory {
-    u64 next[CAP + 1];
+    u32 next[CAP + 1];
 };
 
-const u64 ARRAY[] = {8, 7, 2, 4, 9, 5, 1, 3, 6};
+const u32 ARRAY[] = {8, 7, 2, 4, 9, 5, 1, 3, 6};
 
 #define LEN (sizeof(ARRAY) / sizeof(ARRAY[0]))
 
@@ -33,42 +34,43 @@ i32 main() {
     Memory* memory = reinterpret_cast<Memory*>(calloc(1, sizeof(Memory)));
     EXIT_IF(!memory);
     {
-        u64* next = memory->next;
-        for (u64 i = 0; i < CAP; ++i) {
+        u32* next = memory->next;
+        for (u32 i = 0; i < CAP; ++i) {
             next[i] = i + 1;
         }
         next[0] = ARRAY[0];
         next[CAP] = ARRAY[0];
-        for (u64 i = 0; i < (LEN - 1); ++i) {
+        for (u32 i = 0; i < (LEN - 1); ++i) {
             next[ARRAY[i]] = ARRAY[i + 1];
         }
         next[ARRAY[LEN - 1]] = LEN + 1;
         {
-            for (u64 i = 0; i < (LEN + 3); ++i) {
-                printf(" %lu", next[i]);
+            for (u32 i = 0; i < (LEN + 3); ++i) {
+                printf(" %u", next[i]);
             }
             printf("\n");
         }
         {
-            u64 cur = 0;
-            for (u64 i = 0; i <= 10000000; ++i) {
+            u32 cur = 0;
+            for (u32 i = 0; i <= 10000000; ++i) {
                 cur = next[cur];
-                u64 dst = ((CAP + cur - 2) % CAP) + 1;
-                u64 n1 = next[cur];
-                u64 n2 = next[n1];
-                u64 n3 = next[n2];
+                u32 dst = ((CAP + cur - 2) % CAP) + 1;
+                u32 n1 = next[cur];
+                u32 n2 = next[n1];
+                u32 n3 = next[n2];
                 while ((dst == n1) || (dst == n2) || (dst == n3)) {
                     dst = (dst + CAP - 1) % CAP;
                 }
                 {
-                    u64 t = next[n3];
+                    u32 t = next[n3];
                     next[n3] = next[dst];
                     next[dst] = next[cur];
                     next[cur] = t;
                 }
             }
         }
-        EXIT_IF((next[1] * next[next[1]]) != 170836011000);
+        EXIT_IF((static_cast<u64>(next[1]) *
+                 static_cast<u64>(next[next[1]])) != 170836011000);
     }
     free(memory);
     printf("All good!\n");
