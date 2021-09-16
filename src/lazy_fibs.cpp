685b12b6bd34c5641cc6df88597d20ae7c940b81
albertgoncalves <goncalves.albert@gmail.com>
2021-09-15 22:21:02 -0400

    disentangle data structures

--- src/lazy_fibs.cpp
+++ src/lazy_fibs.cpp
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CAP_LISTS  (1 << 6)
-#define CAP_THUNKS (1 << 6)
-#define CAP_ARGS   (1 << 6)
+#define CAP_LISTS (1 << 6)
+#define CAP_LAZYS (1 << 6)
+#define CAP_ARGS  (1 << 6)
 
 typedef size_t usize;
 
@@ -24,31 +24,28 @@ typedef int64_t i64;
         exit(EXIT_FAILURE);          \
     }
 
-template <typename>
-struct Thunk;
-
 template <typename T>
-struct List {
-    Thunk<T>* thunk;
-    T         value;
-};
-
-template <typename T>
-struct ThunkDefer {
-    List<T>* (*func)(void*);
+struct LazyDefer {
+    T (*func)(void*);
     void* payload;
 };
 
 template <typename T>
-union ThunkBody {
-    ThunkDefer<T> as_defer;
-    List<T>*      as_list;
+union LazyBody {
+    LazyDefer<T> as_defer;
+    T            as_result;
 };
 
 template <typename T>
-struct Thunk {
-    ThunkBody<T> body;
-    bool         cached;
+struct Lazy {
+    LazyBody<T> body;
+    bool        cached;
+};
+
+template <typename T>
+struct List {
+    Lazy<List<T>*>* thunk;
+    T               value;
 };
 
 template <typename>
@@ -68,9 +65,9 @@ struct Buffer {
 
 template <typename T>
 struct Memory {
-    Buffer<List<T>, CAP_LISTS>   lists;
-    Buffer<Thunk<T>, CAP_THUNKS> thunks;
-    Buffer<Args<T>, CAP_ARGS>    args;
+    Buffer<List<T>, CAP_LISTS>        lists;
+    Buffer<Lazy<List<T>*>, CAP_LAZYS> lazys;
+    Buffer<Args<T>, CAP_ARGS>         args;
 };
 
 static List<i64>* FIBS = null;
@@ -82,25 +79,14 @@ static T* alloc(Buffer<T, N>* buffer) {
 }
 
 template <typename T>
-static List<T>* force(Thunk<T>* thunk) {
-    if (thunk->cached) {
-        return thunk->body.as_list;
+static T force(Lazy<T>* lazy) {
+    if (lazy->cached) {
+        return lazy->body.as_result;
     }
-    List<T>* list = thunk->body.as_defer.func(thunk->body.as_defer.payload);
-    thunk->cached = true;
-    thunk->body.as_list = list;
-    return list;
-}
-
-template <typename T>
-static Thunk<T>* defer(Memory<T>* memory,
-                       List<T>* (*func)(void*),
-                       void* payload) {
-    Thunk<T>* thunk = alloc(&memory->thunks);
-    thunk->cached = false;
-    thunk->body.as_defer.func = func;
-    thunk->body.as_defer.payload = payload;
-    return thunk;
+    lazy->cached = true;
+    lazy->body.as_result =
+        lazy->body.as_defer.func(lazy->body.as_defer.payload);
+    return lazy->body.as_result;
 }
 
 template <typename T>
@@ -111,6 +97,14 @@ static T head(List<T>* list) {
 template <typename T>
 static List<T>* tail(List<T>* list) {
     return force(list->thunk);
+}
+
+template <typename T>
+static List<T>* drop(i32 n, List<T>* list) {
+    for (; 0 < n; --n) {
+        list = tail(list);
+    }
+    return list;
 }
 
 template <typename T>
@@ -130,15 +124,10 @@ List<T>* zip_sum(Memory<T>* memory, List<T>* a, List<T>* b) {
     args->memory = memory;
     args->lists[0] = a;
     args->lists[1] = b;
-    list->thunk = defer(memory, f0, reinterpret_cast<void*>(args));
-    return list;
-}
-
-template <typename T>
-static List<T>* drop(i32 n, List<T>* list) {
-    for (; 0 < n; --n) {
-        list = tail(list);
-    }
+    list->thunk = alloc(&memory->lazys);
+    list->thunk->cached = false;
+    list->thunk->body.as_defer.func = f0;
+    list->thunk->body.as_defer.payload = reinterpret_cast<void*>(args);
     return list;
 }
 
@@ -152,22 +141,25 @@ static List<T>* f2(void* payload) {
     Memory<T>* memory = reinterpret_cast<Memory<T>*>(payload);
     List<T>*   list = alloc(&memory->lists);
     list->value = 1;
-    list->thunk = defer(memory, f1, payload);
+    list->thunk = alloc(&memory->lazys);
+    list->thunk->cached = false;
+    list->thunk->body.as_defer.func = f1;
+    list->thunk->body.as_defer.payload = payload;
     return list;
 }
 
 i32 main() {
-    printf("sizeof(List<i64>)       : %zu\n"
-           "sizeof(ThunkDefer<i64>) : %zu\n"
-           "sizeof(ThunkBody<i64>)  : %zu\n"
-           "sizeof(Thunk<i64>)      : %zu\n"
-           "sizeof(Args<i64>)       : %zu\n"
-           "sizeof(Memory<i64>)     : %zu\n"
+    printf("sizeof(List<i64>)             : %zu\n"
+           "sizeof(LazyDefer<List<i64>*>) : %zu\n"
+           "sizeof(LazyBody<List<i64>*>)  : %zu\n"
+           "sizeof(Lazy<List<i64>*>)      : %zu\n"
+           "sizeof(Args<i64>)             : %zu\n"
+           "sizeof(Memory<i64>)           : %zu\n"
            "\n",
            sizeof(List<i64>),
-           sizeof(ThunkDefer<i64>),
-           sizeof(ThunkBody<i64>),
-           sizeof(Thunk<i64>),
+           sizeof(LazyDefer<List<i64>*>),
+           sizeof(LazyBody<List<i64>*>),
+           sizeof(Lazy<List<i64>*>),
            sizeof(Args<i64>),
            sizeof(Memory<i64>));
     Memory<i64>* memory =
@@ -175,7 +167,10 @@ i32 main() {
     {
         FIBS = alloc(&memory->lists);
         FIBS->value = 0;
-        FIBS->thunk = defer(memory, f2, reinterpret_cast<void*>(memory));
+        FIBS->thunk = alloc(&memory->lazys);
+        FIBS->thunk->cached = false;
+        FIBS->thunk->body.as_defer.func = f2;
+        FIBS->thunk->body.as_defer.payload = reinterpret_cast<void*>(memory);
         i64 x = head(drop(50, FIBS));
         printf("%ld\n", x);
         EXIT_IF(x != 12586269025);
