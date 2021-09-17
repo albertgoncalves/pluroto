1d6c9100fd5c1355ece0943518cfc2a9b396a177
albertgoncalves <goncalves.albert@gmail.com>
2021-09-17 17:53:53 -0400

    basic string structure

---
+++ src/string.cpp
@@ -0,0 +1,38 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef size_t usize;
+
+typedef int32_t i32;
+typedef ssize_t isize;
+
+#define STDOUT 1
+
+struct String {
+    const char* chars;
+    usize       len;
+};
+
+#define GET_STRING(literal)  \
+    ((String){               \
+        literal,             \
+        sizeof(literal) - 1, \
+    })
+
+#define EXIT_IF(condition)      \
+    {                           \
+        if (condition) {        \
+            exit(EXIT_FAILURE); \
+        }                       \
+    }
+
+static void print(String string) {
+    EXIT_IF(static_cast<isize>(string.len) !=
+            write(STDOUT, string.chars, string.len));
+}
+
+i32 main() {
+    print(GET_STRING("Hello, world!\n"));
+    return EXIT_SUCCESS;
+}
