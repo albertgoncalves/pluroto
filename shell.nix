8a85e3cc9caa4e7c52d00308e05143df22f5e01e
albertgoncalves <goncalves.albert@gmail.com>
2021-12-30 19:18:19 -0500

    switch to `mold` linker

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 mkShell.override { stdenv = llvmPackages_12.stdenv; } {
     buildInputs = [
-        llvmPackages_12.lld
+        mold
         shellcheck
         valgrind
     ];
