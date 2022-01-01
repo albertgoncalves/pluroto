221d86a3c85ec8245e424a9e80da20ed899e314c
albertgoncalves <goncalves.albert@gmail.com>
2022-01-01 18:26:42 -0500

    read file with mmap

---
+++ src/write_struct.cpp
@@ -0,0 +1,83 @@
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+typedef uint8_t u8;
+typedef int32_t i32;
+typedef double  f64;
+
+#define null nullptr
+
+typedef FILE        File;
+typedef struct stat FileStat;
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+struct Payload {
+    u8   arg0;
+    i32  arg1;
+    f64  arg2;
+    char arg3[5];
+};
+
+#define EXIT_IF(condition)           \
+    if (condition) {                 \
+        fflush(stdout);              \
+        fprintf(stderr,              \
+                "%s:%s:%d \"%s\"\n", \
+                __FILE__,            \
+                __func__,            \
+                __LINE__,            \
+                #condition);         \
+        _exit(EXIT_FAILURE);         \
+    }
+
+i32 main(i32 n, const char** args) {
+    EXIT_IF(n < 2);
+    {
+        Payload payload = {
+            255,
+            -1,
+            10.0,
+            "abcd",
+        };
+        i32 file = open(args[1], O_RDWR | O_CREAT);
+        EXIT_IF(file < 0);
+        void* memory = mmap(null,
+                            sizeof(Payload),
+                            PROT_READ | PROT_WRITE,
+                            MAP_SHARED,
+                            file,
+                            0);
+        close(file);
+        EXIT_IF(memory == MAP_FAILED);
+        memcpy(memory, &payload, sizeof(Payload));
+        msync(memory, sizeof(Payload), MS_SYNC);
+        munmap(memory, sizeof(Payload));
+    }
+    {
+        i32 file = open(args[1], O_RDONLY);
+        EXIT_IF(file < 0);
+        {
+            FileStat stat;
+            EXIT_IF(fstat(file, &stat) < 0)
+            EXIT_IF(stat.st_size != sizeof(Payload));
+        }
+        Payload* memory = reinterpret_cast<Payload*>(
+            mmap(null, sizeof(Payload), PROT_READ, MAP_SHARED, file, 0));
+        close(file);
+        EXIT_IF(memory == MAP_FAILED);
+        printf("%hhu\n%d\n%.1f\n\"%s\"\n",
+               memory->arg0,
+               memory->arg1,
+               memory->arg2,
+               memory->arg3);
+        munmap(memory, sizeof(Payload));
+    }
+    return EXIT_SUCCESS;
+}
