06aa04868e1274dff72919b5407c4a7b5cf5f47d
albertgoncalves <goncalves.albert@gmail.com>
2024-05-07 09:18:53 -0400

    vec demo

---
+++ src/vec.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+static std::ostream& operator<<(std::ostream& stream, std::vector<T>& vec) {
+    size_t n = vec.size() - 1u;
+    for (size_t i = 0; i < n; ++i) {
+        stream << vec[i] << ", ";
+    }
+    stream << vec[n] << '\n';
+    return stream;
+}
+
+int main() {
+    std::vector<int> vec = {};
+    vec.push_back(-123);
+    vec.push_back(2);
+    vec.push_back(3456);
+    vec[1] = -2;
+
+    std::cout << vec;
+}
