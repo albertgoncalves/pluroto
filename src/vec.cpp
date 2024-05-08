193852a00dd57b7bdeb85817f718986fb9535cdf
albertgoncalves <goncalves.albert@gmail.com>
2024-05-07 23:12:43 -0400

    slight adjustment

--- src/vec.cpp
+++ src/vec.cpp
@@ -4,10 +4,12 @@
 template <typename T>
 static std::ostream& operator<<(std::ostream& stream, std::vector<T>& vec) {
     size_t n = vec.size() - 1u;
+
+    stream << '[';
     for (size_t i = 0; i < n; ++i) {
         stream << vec[i] << ", ";
     }
-    stream << vec[n] << '\n';
+    stream << vec[n] << ']';
     return stream;
 }
 
@@ -18,5 +20,5 @@ int main() {
     vec.push_back(3456);
     vec[1] = -2;
 
-    std::cout << vec;
+    std::cout << vec << '\n';
 }
