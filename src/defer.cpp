1344c2b1520478ea4039946acc1642385286235f
albertgoncalves <goncalves.albert@gmail.com>
2024-05-12 22:24:02 -0400

    further refine `defer` demo

--- src/defer.cpp
+++ src/defer.cpp
@@ -1,42 +1,35 @@
 #include <cstdint>
 #include <cstdio>
+#include <functional>
 
 typedef int32_t i32;
 
-#define OK 0
-
-template <typename F>
-struct __Defer__ {
-    F f;
-    __Defer__(F x) : f(x) {
-    }
-    __Defer__(__Defer__&&) = default;
-    ~__Defer__() {
+struct Defer {
+    std::function<void(void)> f;
+    ~Defer() {
         f();
     }
 };
 
-template <typename F>
-__Defer__<F> __defer__(F f) {
-    return __Defer__<F>(f);
-}
+#define IDENT_1(k) __##k##__
+#define IDENT_2(k) IDENT_1(k)
+#define IDENT_3()  IDENT_2(__COUNTER__)
 
-#define __DEFER_1__(k) __##k##__
-#define __DEFER_2__(k) __DEFER_1__(k)
-#define __DEFER_3__()  __DEFER_2__(__COUNTER__)
-
-#define DEFER(block) auto __DEFER_3__() = __defer__([&]() { block; })
+#define defer(block)  \
+    Defer IDENT_3() { \
+        [&]() block   \
+    }
 
 i32 main() {
     {
-        DEFER({ printf("(0) defer(...)\n"); });
+        defer({ printf("(0) defer(...)\n"); });
     }
-    DEFER({
+    defer({
         printf("(4) defer(...)\n");
         printf("(5) defer(...)\n");
     });
     printf("(1) ...\n");
-    DEFER({ printf("(3) defer(...)\n"); });
+    defer({ printf("(3) defer(...)\n"); });
     printf("(2) return ...;\n");
-    return OK;
+    return 0;
 }
